ac2bb95725aacff0eed2b32e8be706bd87bea8c5
Abolfazl <a.jvd.neya.81@gmail.com>
2022-09-05 22:02:18 +0430

    end page about

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import Home from "./Frontend/Pages/Home/Home";
 import Courses from "./Frontend/Pages/Courses/Courses";
 import Comments from "./Frontend/Pages/Comments/Comments";
+import About from "./Frontend/Pages/About/About";
 
 const AllRoutes = [
   { path: "/", element: <Home /> },
   { path: "/courses", element: <Courses /> },
   { path: "/comments", element: <Comments /> },
+  { path: "/about", element: <About /> },
 ];
 
 export default AllRoutes;
